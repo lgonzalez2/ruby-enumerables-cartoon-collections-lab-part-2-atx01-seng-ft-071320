e11e50f29af6a8eed91857db8052fbe8fbd28881
Lucio Gonzalez <lgonzalez_94@hotmail.com>
2020-07-05 14:04:47 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,9 +25,7 @@ def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   
   planeteer_calls.find do |call|
-    
     valid_calls.include?(call) 
-    
   end 
 end
 
