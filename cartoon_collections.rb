a867c7bae9cd166a6ef7c547c7b8a30bf7c113c9
Lucio Gonzalez <lgonzalez_94@hotmail.com>
2020-07-05 14:23:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,11 +20,3 @@ end
 
 
 
-def find_valid_calls(planeteer_calls)
-  valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  
-  planeteer_calls.find do |call|
-    valid_calls.include?(call) 
-  end 
-end
-
